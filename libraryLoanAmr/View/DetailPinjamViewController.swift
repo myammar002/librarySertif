46c37e6f739251c5dfd59e001beb8e66cc5967e1
myammar002 <myammar002@gmail.com>
2022-05-08 16:54:54 +0700

    update pinjam

--- libraryLoanAmr/View/DetailPinjamViewController.swift
+++ libraryLoanAmr/View/DetailPinjamViewController.swift
@@ -13,19 +13,36 @@ class DetailPinjamViewController: UIViewController {
     @IBOutlet weak var buku_text: UITextField!
     @IBOutlet weak var datePinjam: UIDatePicker!
     @IBOutlet weak var dateKembali: UILabel!
+    @IBOutlet weak var tanggalPinjam: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+      
         // Do any additional setup after loading the view.
     }
     
     @IBAction func btnsave(_ sender: UIButton) {
+        
     }
     
     @IBAction func btncancel(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
     
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        var dateComponent = DateComponents()
+        dateComponent.day = 7
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+
+        let strDate = dateFormatter.string(from: datePinjam.date)
+        let returnDate = Calendar.current.date(byAdding: dateComponent, to: datePinjam.date)
+        let strReturn = dateFormatter.string(from: returnDate!)
+        
+        tanggalPinjam.text = strDate
+        dateKembali.text = strReturn
+    }
     
 }
